65bff55800d17589752691b520e3f91ada814642
Silince <837418086@qq.com>
2020-11-19 23:38:33 +0800

    2020-11-19 23:38 update code

--- cloud-provider-payment8001/src/main/java/cn/silince/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/silince/springcloud/PaymentMain8001.java
@@ -1,5 +1,7 @@
 package cn.silince.springcloud;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * @program: cloud2020
  * @description:
@@ -7,5 +9,7 @@ package cn.silince.springcloud;
  * @create: 2020-11-19 22:51
  **/
 public class PaymentMain8001 {
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
 }

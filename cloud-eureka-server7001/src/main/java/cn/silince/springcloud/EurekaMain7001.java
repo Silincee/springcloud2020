8cd127550a3f54b49cf5f90c2132d35a0bb0363b
Silince <837418086@qq.com>
2020-11-21 22:33:25 +0800

    2020-11-21 22:33 update code

---
+++ cloud-eureka-server7001/src/main/java/cn/silince/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package cn.silince.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: cloud2020
+ * @description:
+ * @author: Silince
+ * @create: 2020-11-21 22:29
+ **/
+@SpringBootApplication
+@EnableEurekaServer // 表示当前是 Eureka的服务注册中心
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}

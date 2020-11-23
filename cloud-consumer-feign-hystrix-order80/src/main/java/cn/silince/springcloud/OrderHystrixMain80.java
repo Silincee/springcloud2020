29cfc9cb19d37128267fa037ba179b6f8773e901
Silince <837418086@qq.com>
2020-11-23 21:34:17 +0800

    2020-11-23 21:34 update code

--- cloud-consumer-feign-hystrix-order80/src/main/java/cn/silince/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/cn/silince/springcloud/OrderHystrixMain80.java
@@ -2,6 +2,7 @@ package cn.silince.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -13,7 +14,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  **/
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixMain80.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/cn/silince/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/silince/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.silince.springcloud.service.impl;
 
 import cn.silince.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,11 +24,20 @@ public class PaymentServiceImpl implements PaymentService {
         return "线程池: "+ Thread.currentThread().getName()+"  paymentInfo_OK: "+id+"\t" + "😄";
     }
 
+    // 设置自身调用超时时间的峰值，峰值内可以正常运行，超过了需要有兜底的方法处理，作服务降级fallback
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "3000") // 该线程超时时间为3s
+    })
     @Override
     public String paymentInfo_TimeOut(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 5; // 5>3
+//        int age = 10/0;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);} catch (InterruptedException e) {e.printStackTrace();}
         return "线程池: "+ Thread.currentThread().getName()+"  paymentInfo_TimeOut: "+id+"\t" + "😭耗时(s): "+timeNumber;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池: "+ Thread.currentThread().getName()+"  paymentInfo_TimeOutHandler: "+id+"\t" + "😈Here is TimeOutHandler";
     }
 }

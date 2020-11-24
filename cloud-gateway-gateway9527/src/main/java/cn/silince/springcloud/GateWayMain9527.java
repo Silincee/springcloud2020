94753f330032e380ad9c5a8a2c6ecff9a2a8704a
Silince <837418086@qq.com>
2020-11-24 15:13:00 +0800

    2020-11-24 15:12 update code

---
+++ cloud-gateway-gateway9527/src/main/java/cn/silince/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package cn.silince.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2020
+ * @description:
+ * @author: Silince
+ * @create: 2020-11-24 14:47
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}

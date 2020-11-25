b19f19bb8a986fb212139f0620072e595bf1abf3
Silince <837418086@qq.com>
2020-11-25 21:41:41 +0800

    2020-11-25 21:41 update code

---
+++ cloud-config-client-3355/src/main/java/cn/silince/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package cn.silince.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: cloud2020
+ * @description: 从配置中心读取内容
+ * @author: Silince
+ * @create: 2020-11-25 19:48
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}

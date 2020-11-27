f6028e2d92b6ef248492cd548bbc0c1e05603a85
Silince <837418086@qq.com>
2020-11-27 21:43:04 +0800

    2020-11-27 21:43 update code

---
+++ cloudalibaba-provider-payment9001/src/main/java/cn/silince/springcloud/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package cn.silince.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: cloud2020
+ * @description:
+ * @author: Silince
+ * @create: 2020-11-27 21:37
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}

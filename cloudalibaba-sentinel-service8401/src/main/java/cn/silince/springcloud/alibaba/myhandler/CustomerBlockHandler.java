b3f9715c1a3b79827389f16bbebf605f50c91478
Silince <837418086@qq.com>
2020-11-29 21:11:30 +0800

    2020-11-29 21:11 update code

---
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/silince/springcloud/alibaba/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,10 @@
+package cn.silince.springcloud.alibaba.myhandler;
+
+/**
+ * @program: cloud2020
+ * @description:
+ * @author: Silince
+ * @create: 2020-11-29 20:31
+ **/
+public class CustomerBlockHandler {
+}
